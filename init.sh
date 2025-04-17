ed6b5a51110f9cb65e640830f662db24daf9afc8
Spyros Alepakos <salepakos@allweb.gr>
2025-04-18 00:43:08 +0300

    123

--- init.sh
+++ init.sh
@@ -43,6 +43,8 @@ sed -i 's/\r$//' devfiles/.bashrc
 
 mv devfiles/nvim ~/.config/
 mv devfiles/.tmux.conf ~
+rm /root/.bashrc
+mv devfiles/.bashrc ~
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 nvim --version
