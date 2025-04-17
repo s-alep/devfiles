9eae4ee005073f0db57ff08aa6a384ba9b5ad0f3
Spyros Alepakos <salepakos@allweb.gr>
2025-04-17 20:54:04 +0300

    123

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,22 +1,11 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
-
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
     require('mini.sessions').setup {
       vim.keymap.set('n', '<leader>tl', '<CMD>lua MiniSessions.read()<CR>', { desc = '[T]oggle Session [L]oad' }),
+      vim.keymap.set('n', '<leader>ts', '<CMD>mksession<CR>', { desc = '[T]oggle Session [S]tart' }),
     }
     require('mini.move').setup()
     require('mini.pairs').setup()
@@ -27,9 +16,6 @@ return { -- Collection of various small independent plugins/modules
         join = '',
       },
     }
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
